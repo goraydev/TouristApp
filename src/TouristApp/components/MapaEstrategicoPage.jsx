de7af6902ef066da289dc16361eed77683e44f35
5kyf4ll <pablito_4-8@hotmail.com>
2022-10-17 13:35:41 -0500

    Ingresando el mapa estrategico

--- src/TouristApp/components/MapaEstrategicoPage.jsx
+++ src/TouristApp/components/MapaEstrategicoPage.jsx
@@ -1,10 +1,31 @@
 const MapaEstrategicoPage = () => {
   return (
-    <div>
-      <h1 className="text-2xl text-center mt-5">
-        Mapa estratégico - Tourist App
-      </h1>
+    <>
+    <link rel="stylesheet" href="src/css/main.min.css"></link>  
+    <body class="Backgrounf-color=yellow">
+    
+    <div class="container">
+    <div class="row" id="titulo">
+            <div class="col-sm-12">
+              <h1 class="">Mapa Estratégico / <b>2016 - 2021</b> / Defensoria Pública do RS</h1>
+            </div>
+          </div>
+      <div class="fundo-branco row-eq-height row" id="linha-um">
+        <div class="cabecalho col-lg-2 col-md-12">
+          <img src="public/logota.png" alt="logo" id="logo"/>
+        </div>
+        <div class="cabecalho col-lg-4 col-md-12">
+          <h2>Misión</h2>
+          <p>Somos una marca aventurera, que impulsa el descubrimiento, la exploración, los viajes y el descubrir nuevas emociones. </p>
+        </div>
+        <div class="cabecalho col-lg-6 col-md-12">
+          <h2>Visión</h2>
+          <p>Ser una empresa intermediaria en el turismo vivencial proporcionado por una persona local, quien lleva al turista a obtener mayores experiencias enriquecedoras de la ciudad que visitará </p>
+        </div>
+      </div>
     </div>
+    </body>
+     </>
   );
 };
 
